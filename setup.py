4f4db4bdeaa83326944a3a4c573b8e5daad82b46
Nicolas Ford <univnf@gmail.com>
2020-04-04 16:57:46 +0100

    fixed licence type

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/nicford/Pubmed-Pipeline',
-    license='MIT',
+    license='GPLv3',
     install_requires=['pyspark==2.4.4', 'joblib==0.14.1', 'nltk==3.4.5', 'numpy==1.18.1', 'pandas==1.0.1', 'pyarrow==0.11', 'requests==2.23.0',
     'scikit-learn==0.22.1', 'scispacy==0.2.4', 'spacy==2.2.3', 'unidecode==1.1.1', 'xgboost==0.90', 'pubmed_parser @ git+git://github.com/titipata/pubmed_parser.git@40d361a756a29cd943a54313c09b95044767eb97'],
     packages=setuptools.find_packages(),
